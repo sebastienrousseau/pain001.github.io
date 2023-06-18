ec8d467b96c1c5e89180cb1b4ad0ba9bf01cfd5e
Sebastien Rousseau <sebastienrousseau@users.noreply.github.com>
2023-06-18 10:57:07 +0100

    fix(pain001): service worker put method

--- templates/service-worker.js
+++ templates/service-worker.js
@@ -1,11 +1,11 @@
 "use strict";
 
 function clearOldCaches() {
-    return debug("Clean old caches"), caches.keys().then(e => Promise.all(e.filter(e => 0 !== e.indexOf(offlineCache)).map(e => caches["delete"](e))))
+    return debug("Clean old caches"), caches.keys().then(e => Promise.all(e.filter(e => 0 !== e.indexOf(offlineCache)).map(e => caches.delete(e))))
 }
 
 function cacheOfflinePage() {
-    return debug("Cache offline page"), caches.open(offlineCache).then(e => e.addAll([offlinePage]))["catch"](e => {
+    return debug("Cache offline page"), caches.open(offlineCache).then(e => e.addAll([offlinePage])).catch(e => {
         debug(e)
     })
 }
@@ -23,16 +23,44 @@ self.addEventListener("install", e => {
     debug("Activating Service Worker"), e.waitUntil(clearOldCaches().then(() => self.clients.claim()))
 }), self.addEventListener("fetch", e => {
     let n = e.request;
-    "GET" === n.method ? e.respondWith(caches.match(n).then(e => (debug("Fetching " + n.url), e && debug("Found in cache: " + n.url), e || fetch(n).then(e => {
-        if (debug("Going to network: " + n.url), e && e.ok) {
-            debug("Saving in cache: " + n.url);
-            let t = e.clone();
-            caches.open(offlineCache).then(e => e.put(n, t))
+    if (n.url.startsWith(self.location.origin)) { // Added this check to avoid caching chrome-extension requests
+        if ("GET" === n.method) {
+            e.respondWith(
+                caches.match(n).then(cachedResponse => {
+                    if (cachedResponse) {
+                        debug("Fetching " + n.url);
+                        debug("Found in cache: " + n.url);
+                        return cachedResponse;
+                    }
+
+                    debug("Going to network: " + n.url);
+                    return fetch(n).then(response => {
+                        if (response && response.ok) {
+                            debug("Saving in cache: " + n.url);
+                            let clonedResponse = response.clone();
+                            caches.open(offlineCache).then(cache => cache.put(n, clonedResponse));
+                        }
+                        return response;
+                    }).catch(error => {
+                        debug("Offline and no cache for: " + n.url + ": " + error);
+                        if (n.mode === "navigate") {
+                            debug("Showing offline page");
+                            return caches.match(offlinePage);
+                        } else if (n.headers.get("Accept").includes("image")) {
+                            return new Response(
+                                '<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>',
+                                {
+                                    headers: {
+                                        "Content-Type": "image/svg+xml"
+                                    }
+                                }
+                            );
+                        }
+                    });
+                })
+            );
+        } else {
+            debug("Ignoring non-GET request");
         }
-        return e
-    })["catch"](e => (debug("Offline and no cache for: " + n.url + ": " + e), "navigate" == n.mode ? (debug("Showing offline page"), caches.match(offlinePage)) : -1 !== n.headers.get("Accept").indexOf("image") ? new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg"><title id="offline-title">Offline</title><g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/><text fill="#9B9B9B" font-family="Helvetica Neue,Arial,Helvetica,sans-serif" font-size="72" font-weight="bold"><tspan x="93" y="172">offline</tspan></text></g></svg>', {
-        headers: {
-            "Content-Type": "image/svg+xml"
-        }
-    }) : void 0))))) : debug("Ignoring non GET request")
+    }
 });
